e88295342f660447e91bc18e11915499750793aa
Edgar <edgar_hg20@hotmail.com>
2016-10-30 21:14:08 -0700

    Agregado listado de reportes en inicio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
 	before_filter :authenticate_user!
   	
   	def index
-  		
+  		if current_user.is_admin?
+  			@reports = Report.all
+  		end
   	end
 end
