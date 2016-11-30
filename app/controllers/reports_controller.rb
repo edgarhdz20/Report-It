8f044daabc5e7162c411f117207b9f4b9ea9fcd2
Edgar <edgar_hg20@hotmail.com>
2016-11-30 00:26:16 -0700

    Cambios

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,13 +8,13 @@ class ReportsController < ApplicationController
   # GET /reports.json
   def index
     if current_user.is_admin?
-      @reports = Report.all.order(created_at: :desc).limit(10)
+      @reports = Report.all.order(created_at: :desc)
     elsif current_user.is_jmas?
-      @reports = Report.where(:report_type_id => 1).order(created_at: :desc).limit(10)
+      @reports = Report.where(:report_type_id => 1).order(created_at: :desc)
     elsif current_user.is_municipio?
-      @reports = Report.where(:report_type_id => 2).order(created_at: :desc).limit(10)
+      @reports = Report.where(:report_type_id => 2).order(created_at: :desc)
     elsif current_user.is_cfe?
-      @reports = Report.where(:report_type_id => 3).order(created_at: :desc).limit(10)
+      @reports = Report.where(:report_type_id => 3).order(created_at: :desc)
     end
   end
 
